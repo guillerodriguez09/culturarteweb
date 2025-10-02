228bf7999d46db946b1f8dc1e90784a0e6b47959
guillerodriguez09 <rodriguezguillermina816@gmail.com>
2025-10-02 14:27:39 -0300

    archivos de prueba

---
+++ src/main/java/com/culturarte/web/PruebaServlet.java
@@ -0,0 +1,26 @@
+package com.culturarte.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/prueba")
+public class PruebaServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+
+        out.println("<html><body>");
+        out.println("<h1>Servlet funcionando 🎉</h1>");
+        out.println("</body></html>");
+    }
+}
+
