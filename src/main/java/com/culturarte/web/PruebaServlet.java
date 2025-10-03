97cfc86cbab7727c0f40e894b617f55f9bad37ba
guillerodriguez09 <rodriguezguillermina816@gmail.com>
2025-10-02 22:22:55 -0300

    pruebas 2

--- src/main/java/com/culturarte/web/PruebaServlet.java
+++ src/main/java/com/culturarte/web/PruebaServlet.java
@@ -1,5 +1,9 @@
 package com.culturarte.web;
 
+import com.culturarte.logica.controllers.IPropuestaController;
+import com.culturarte.logica.dtos.DTOPropuesta;
+import com.culturarte.logica.fabrica.Fabrica;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 @WebServlet("/prueba")
 public class PruebaServlet extends HttpServlet {
@@ -18,9 +23,28 @@ public class PruebaServlet extends HttpServlet {
         resp.setContentType("text/html;charset=UTF-8");
         PrintWriter out = resp.getWriter();
 
-        out.println("<html><body>");
-        out.println("<h1>Servlet funcionando 🎉</h1>");
-        out.println("</body></html>");
+        try {
+            // Obtener el controller desde el JAR del central
+            IPropuestaController ctrl = Fabrica.getInstancia().getPropuestaController();
+            List<DTOPropuesta> propuestas = ctrl.listarPropuestasConProponente();
+
+            out.println("<html><body>");
+            out.println("<h1> Servlet funcionando con dependencia al central</h1>");
+            out.println("<p>Cantidad de propuestas registradas: " + propuestas.size() + "</p>");
+
+            if (!propuestas.isEmpty()) {
+                out.println("<ul>");
+                for (DTOPropuesta p : propuestas) {
+                    out.println("<li>" + p.getTitulo() + " (" + p.getEstadoActual() + ")</li>");
+                }
+                out.println("</ul>");
+            }
+
+            out.println("</body></html>");
+        } catch (Exception e) {
+            out.println("<p style='color:red'>⚠ Error: " + e.getMessage() + "</p>");
+            e.printStackTrace();
+        }
     }
 }
 
